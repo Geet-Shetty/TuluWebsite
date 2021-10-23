e81181453c16c7674b11a188d9a1043b37a70718
Geet Shetty <gshetty902@gmail.com>
2021-10-22 17:54:21 -0700

    basic backend with id search

---
+++ backend/index.js
@@ -0,0 +1,55 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const Word = require("./models/word");
+
+app.use(express.static("build"));
+app.use(cors());
+app.use(express.json());
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
+
+app.get("/api/words/:id", (request, response, next) => {
+  Word.findById(request.params.id)
+    .then((word) => {
+      if (word) {
+        response.json(word); // found and returing
+      } else {
+        response.status(404).end(); // not found with correctly formatted id
+      }
+    })
+    .catch((error) => {
+      console.log("WAT IS THIS");
+      next(error);
+    });
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+app.use(errorHandler);
+
+const PORT = 3001;
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
+
+// {$expr: {$eq: ["$word.kannada", "$word.tulu"] } }
+// {"word.english": "goovu"}
---
+++ backend/models/word.js
@@ -0,0 +1,75 @@
+const mongoose = require("mongoose");
+
+const dotenv = require("dotenv");
+dotenv.config("./env");
+
+const url = process.env.DB_URL;
+
+let conn = mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
+
+const wordSchema = new mongoose.Schema({
+  word: {
+    kannada: String,
+    english: String,
+    tulu: String,
+    origin: String,
+    id: Number,
+  },
+  meanings: [
+    {
+      contexts: [String],
+      linked_context: {
+        context: String,
+        kannada: String,
+        english: String,
+        tulu: String,
+        id: Number,
+      },
+      definitions: [String],
+    },
+  ],
+  variations: [
+    {
+      origin: String,
+      words: [
+        {
+          kannada: String,
+          english: String,
+          tulu: String,
+          id: Number,
+        },
+      ],
+    },
+  ],
+  references: [
+    {
+      reference: String,
+      sentences: [
+        {
+          tulu: String,
+          english: String,
+          kannada: String,
+        },
+      ],
+    },
+  ],
+  examples: [String],
+  language_references: String,
+});
+
+wordSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    // delete returnedObject._id;
+    // delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Tulu", wordSchema);
