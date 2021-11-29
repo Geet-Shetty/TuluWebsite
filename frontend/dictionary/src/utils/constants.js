dbde7dacd96a253f31d413440c2b23e8ac2a4576
Brian-Chung1 <brian.chung.cs@gmail.com>
2021-11-28 23:12:17 -0800

    brians fix

---
+++ frontend/dictionary/src/utils/constants.js
@@ -0,0 +1,8 @@
+export const DEFAULT_EMPTY_WORD = {
+  word: undefined,
+  meanings: undefined,
+  variations: undefined,
+  references: undefined,
+  examples: undefined,
+  language_refs: undefined,
+};
